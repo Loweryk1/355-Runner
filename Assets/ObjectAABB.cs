b32542876f4c74c6f1252bbddf876096c6448818
Kyle Lowery <Loweryk1@ferris.edu>
2018-09-06 13:20:43 -0400

    Add a template for AABB collision
    Need help figuring out how to call and retrieve information from GameObjects using code.

---
+++ Assets/ObjectAABB.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAABB : MonoBehaviour {
+
+    float width;
+    float height;
+    float depth;
+
+	// Use this for initialization
+	void Start () {     //Pass in the object?
+		//Store GameObject's width (x) under width.
+        //Store GameObject's height (y) under height.
+        //Store GameObject's depth (z) under depth.
+	}
+	
+	// Update is called once per frame
+	void checkForCollision () {     //Pass in an AABB for another GameObject to check for collision.   
+        //If this GameObject's x position is in the other GameObject's x position, return true.
+        //If this GameObject's y position is in the other GameObject's y position, return true.
+        //If this GameObject's z position is in the other GameObject's z position, return true.
+        return false;
+	}
+
+    
+}
